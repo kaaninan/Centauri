6398dbbd7c68ee426e46dceb17207fa33b8ed05b
Kağan İNAN <iletisim@kaaninan.net>
2017-04-11 21:07:10 +0300

    .gitignore is now working

--- Software/ROS/build/centauri/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/Centauri/Software/ROS/src/centauri
-# Build directory: /home/robot/Centauri/Software/ROS/build/centauri
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Software/ROS/build/centauri_sound/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/Centauri/Software/ROS/src/centauri_sound
-# Build directory: /home/robot/Centauri/Software/ROS/build/centauri_sound
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
