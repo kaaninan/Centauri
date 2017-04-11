d3475786a579a7a232ee5291776a52acb38cda6c
Kaan Inan <iletisim@kaaninan.net>
2017-04-11 20:42:05 +0300

    ros packages

---
+++ Software/ROS/build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/Centauri/Software/ROS/src
+# Build directory: /home/robot/Centauri/Software/ROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(centauri)
+subdirs(centauri_bridge)
+subdirs(centauri_cam)
+subdirs(centauri_controller)
+subdirs(centauri_interface)
+subdirs(centauri_osc)
+subdirs(centauri_sound)
+subdirs(centauri_start)
---
+++ Software/ROS/build/centauri_cam/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/Centauri/Software/ROS/src/centauri_cam
+# Build directory: /home/robot/Centauri/Software/ROS/build/centauri_cam
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
